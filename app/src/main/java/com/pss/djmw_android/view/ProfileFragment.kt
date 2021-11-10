fa5663fca0076108157d6f1db1035e7fae2f0130
ParkSangSun <appleasz1028102@gmail.com>
2021-11-10 17:02:02 +0900

    create : ProfileFragment.kt

---
+++ app/src/main/java/com/pss/djmw_android/view/ProfileFragment.kt
@@ -0,0 +1,17 @@
+package com.pss.djmw_android.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.djmw_android.R
+import com.pss.djmw_android.base.BaseFragment
+import com.pss.djmw_android.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    override fun init() {
+
+    }
+}
