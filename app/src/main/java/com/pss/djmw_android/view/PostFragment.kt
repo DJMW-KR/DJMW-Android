3a2a58fe2f81bed4b8bf8ca4627ac61d25c1138f
ParkSangSun <appleasz1028102@gmail.com>
2021-11-10 17:02:17 +0900

    create : PostFragment.kt

---
+++ app/src/main/java/com/pss/djmw_android/view/PostFragment.kt
@@ -0,0 +1,16 @@
+package com.pss.djmw_android.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.djmw_android.R
+import com.pss.djmw_android.base.BaseFragment
+import com.pss.djmw_android.databinding.FragmentPostBinding
+
+class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
+    override fun init() {
+
+    }
+}
