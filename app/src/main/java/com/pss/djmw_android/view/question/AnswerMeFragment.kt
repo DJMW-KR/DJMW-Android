d5574ec28e9835d9668ae7c662009c4d49763673
ParkSangSun <appleasz1028102@gmail.com>
2021-11-11 14:00:50 +0900

    add : answerMeFragment, RecyclerView

--- app/src/main/java/com/pss/djmw_android/view/question/AnswerMeFragment.kt
+++ app/src/main/java/com/pss/djmw_android/view/question/AnswerMeFragment.kt
@@ -8,10 +8,17 @@ import android.view.ViewGroup
 import com.pss.djmw_android.R
 import com.pss.djmw_android.base.BaseFragment
 import com.pss.djmw_android.databinding.FragmentAnswerMeBinding
+import com.pss.djmw_android.view.question.adapter.AnswerRecyclerViewAdapter
+import com.pss.djmw_android.widget.extension.showVertical
 
 
 class AnswerMeFragment : BaseFragment<FragmentAnswerMeBinding>(R.layout.fragment_answer_me) {
     override fun init() {
+        initRecyclerView()
+    }
 
+    private fun initRecyclerView() {
+        binding.answerMeRecyclerView.showVertical(requireContext())
+        binding.answerMeRecyclerView.adapter = AnswerRecyclerViewAdapter()
     }
 }
