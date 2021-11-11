7a9ddbfa0a43cf87211f8bb8f1e071a9c5fc5b5b
ParkSangSun <appleasz1028102@gmail.com>
2021-11-11 09:45:37 +0900

    create : AnswerMeFragment.kt

---
+++ app/src/main/java/com/pss/djmw_android/view/question/AnswerMeFragment.kt
@@ -0,0 +1,17 @@
+package com.pss.djmw_android.view.question
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.djmw_android.R
+import com.pss.djmw_android.base.BaseFragment
+import com.pss.djmw_android.databinding.FragmentAnswerMeBinding
+
+
+class AnswerMeFragment : BaseFragment<FragmentAnswerMeBinding>(R.layout.fragment_answer_me) {
+    override fun init() {
+
+    }
+}
