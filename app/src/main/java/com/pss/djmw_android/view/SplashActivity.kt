68039ee9e28049370c73c1803b2b5cbd6aa146a8
ParkSangSun <appleasz1028102@gmail.com>
2021-11-12 14:17:50 +0900

    add : check user kakao login

--- app/src/main/java/com/pss/djmw_android/view/SplashActivity.kt
+++ app/src/main/java/com/pss/djmw_android/view/SplashActivity.kt
@@ -7,24 +7,55 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import com.kakao.sdk.auth.AuthApiClient
+import com.kakao.sdk.common.model.KakaoSdkError
 import com.kakao.sdk.common.util.Utility.getKeyHash
+import com.kakao.sdk.user.UserApiClient
 import com.pss.djmw_android.R
 import com.pss.djmw_android.base.BaseActivity
 import com.pss.djmw_android.databinding.ActivitySplashBinding
+import com.pss.djmw_android.view.main.MainActivity
+import com.pss.djmw_android.view.signin.SignInActivity
+import com.pss.djmw_android.widget.extension.startActivityWithFinish
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
     override fun init() {
         checkSdkVersion()
-        Log.d("TAG",getKeyHash(applicationContext))
+        checkKakaoLogin()
     }
 
-    private fun checkSdkVersion(){
+    private fun checkKakaoLogin() {
+        if (AuthApiClient.instance.hasToken()) {
+            UserApiClient.instance.accessTokenInfo { _, error ->
+                if (error != null) {
+                    if (error is KakaoSdkError && error.isInvalidTokenError()) {
+                        //로그인 필요
+                        startSignIn()
+                    } else {
+                        //기타 에러
+                        longShowToast("에러가 발생했습니다, 오류문자 : ${error.message}")
+                    }
+                } else {
+                    //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+                    this.startActivityWithFinish(this,MainActivity::class.java)
+                }
+            }
+        } else {
+            //로그인 필요
+            startSignIn()
+        }
+    }
+
+    private fun startSignIn() = this.startActivityWithFinish(this, SignInActivity::class.java)
+
+    private fun checkSdkVersion() {
         if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
             setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
         }
         if (Build.VERSION.SDK_INT >= 19) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
         if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
