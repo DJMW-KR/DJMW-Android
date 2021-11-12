05c1faa6f4cb33d11cdbe4673cfbeed5a3de5bb8
ParkSangSun <appleasz1028102@gmail.com>
2021-11-12 14:14:53 +0900

    add : di app

--- app/src/main/java/com/pss/djmw_android/di/App.kt
+++ app/src/main/java/com/pss/djmw_android/di/App.kt
@@ -3,8 +3,9 @@ package com.pss.djmw_android.di
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import com.pss.djmw_android.widget.utils.KakaoKey.KAKAO_NATIVE_APP_KEY
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class App : Application() {
     private var instance: App? = null
 
