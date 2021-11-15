b51b73534cab868a7095375e25fa9f433f15d852
ParkSangSun <appleasz1028102@gmail.com>
2021-11-15 09:58:01 +0900

    add : SignInRepository.kt

---
+++ app/src/main/java/com/pss/djmw_android/repository/SignInRepository.kt
@@ -0,0 +1,12 @@
+package com.pss.djmw_android.repository
+
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import javax.inject.Inject
+
+class SignInRepository @Inject constructor(
+    private val firebaseDatabase: FirebaseDatabase,
+    private val firestore: FirebaseFirestore
+) {
+    fun versionCheck() = firebaseDatabase.reference.child("version")
+}
