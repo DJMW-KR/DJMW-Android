418536f1bef49c2cffb1a113d7f51e63ca3b4704
ParkSangSun <appleasz1028102@gmail.com>
2021-11-29 11:56:32 +0900

    update : UserInfo.kt

--- app/src/main/java/com/pss/djmw_android/data/model/UserInfo.kt
+++ app/src/main/java/com/pss/djmw_android/data/model/UserInfo.kt
@@ -2,5 +2,9 @@ package com.pss.djmw_android.data.model
 
 data class UserInfo(
     val userName : String,
-    val userId : String
-)
+    val userId : String,
+    val answerQuestion : String,
+    val participationQuestion : String
+){
+    constructor() : this("오류","오류","0","0")
+}
