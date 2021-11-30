e085caf8b1fd9ca567c34d713c1324b48288c712
ParkSangSun <appleasz1028102@gmail.com>
2021-11-30 09:15:40 +0900

    add : UserInfo parcelable

--- app/src/main/java/com/pss/djmw_android/data/model/UserInfo.kt
+++ app/src/main/java/com/pss/djmw_android/data/model/UserInfo.kt
@@ -1,10 +1,14 @@
 package com.pss.djmw_android.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class UserInfo(
     val userName : String,
     val userId : String,
     val answerQuestion : String,
     val participationQuestion : String
-){
+) : Parcelable {
     constructor() : this("오류","오류","0","0")
 }
