97b0afa100444da231126edebacfc53e120982b9
ParkSangSun <appleasz1028102@gmail.com>
2021-12-07 15:53:06 +0900

    add : PostViewModel

--- app/src/main/java/com/pss/djmw_android/view/main/post/PostFragment.kt
+++ app/src/main/java/com/pss/djmw_android/view/main/post/PostFragment.kt
@@ -6,16 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.pss.djmw_android.R
 import com.pss.djmw_android.base.BaseFragment
 import com.pss.djmw_android.databinding.FragmentPostBinding
 import com.pss.djmw_android.view.main.post.adapter.PostRecyclerViewAdapter
 import com.pss.djmw_android.view.main.question.adapter.AnswerRecyclerViewAdapter
+import com.pss.djmw_android.viewmodel.PostViewModel
 import com.pss.djmw_android.widget.extension.showVertical
 
 class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
+    private val postViewModel by activityViewModels<PostViewModel>()
+
+
     override fun init() {
         initRecyclerView()
+        observeViewModel()
+        initGet()
+    }
+
+    private fun observeViewModel(){
+        postViewModel.eventGetPostResponse.observe(this,{
+            Log.d("로그","Post 가져온 값 : $it")
+        })
+    }
+
+    private fun initGet(){
+        postViewModel.getPost()
     }
 
     private fun initRecyclerView() {
---
+++ app/src/main/java/com/pss/djmw_android/viewmodel/PostViewModel.kt
@@ -0,0 +1,30 @@
+package com.pss.djmw_android.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.pss.djmw_android.repository.PostRepository
+import com.pss.djmw_android.repository.SignInRepository
+import com.pss.djmw_android.widget.utils.DataStoreModule
+import com.pss.djmw_android.widget.utils.SingleLiveEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PostViewModel  @Inject constructor(
+    private val postRepository: PostRepository
+) : ViewModel() {
+    val eventGetPostResponse: LiveData<Any> get() = _eventGetPostResponse
+    private val _eventGetPostResponse = SingleLiveEvent<Any>()
+
+    val eventError: LiveData<Int> get() = _eventError
+    private val _eventError = SingleLiveEvent<Int>()
+
+
+    fun getPost()= postRepository.getPost()
+        .addOnSuccessListener {
+            _eventGetPostResponse.postValue(it.value)
+        }
+        .addOnFailureListener {
+            _eventError.postValue(0)
+        }
+}
