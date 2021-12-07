adca8b8252d297d0c6c36114f9763d8e05314625
ParkSangSun <appleasz1028102@gmail.com>
2021-12-07 17:05:38 +0900

    add : Visibility setting extension

--- app/src/main/java/com/pss/djmw_android/widget/extension/FragmentExtension.kt
+++ app/src/main/java/com/pss/djmw_android/widget/extension/FragmentExtension.kt
@@ -1,6 +1,9 @@
 package com.pss.djmw_android.widget.extension
 
 import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,4 +16,20 @@ fun RecyclerView.showVertical(context: Context){
 fun RecyclerView.showHorizontal(context: Context){
     this.layoutManager =
         LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+}
+
+fun TextView.gone(){
+    this.visibility = View.GONE
+}
+
+fun TextView.visible(){
+    this.visibility = View.VISIBLE
+}
+
+fun ImageView.gone(){
+    this.visibility = View.GONE
+}
+
+fun ImageView.visible(){
+    this.visibility = View.VISIBLE
 }
