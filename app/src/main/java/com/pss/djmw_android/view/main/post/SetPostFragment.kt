3226ca1b9c5a3a28f1ce21261c773c68fa94dd10
ParkSangSun <appleasz1028102@gmail.com>
2021-12-08 10:58:15 +0900

    create : SetPostFragment.kt

---
+++ app/src/main/java/com/pss/djmw_android/view/main/post/SetPostFragment.kt
@@ -0,0 +1,18 @@
+package com.pss.djmw_android.view.main.post
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.djmw_android.R
+import com.pss.djmw_android.base.BaseFragment
+import com.pss.djmw_android.databinding.FragmentSetPostBinding
+
+
+class SetPostFragment : BaseFragment<FragmentSetPostBinding>(R.layout.fragment_set_post) {
+
+    override fun init() {
+
+    }
+}
