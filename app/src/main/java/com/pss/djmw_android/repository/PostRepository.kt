3e1cbf175a73606ffef6e270f2f807d393e079ec
ParkSangSun <appleasz1028102@gmail.com>
2021-12-08 17:46:59 +0900

    update : post orderBy

--- app/src/main/java/com/pss/djmw_android/repository/PostRepository.kt
+++ app/src/main/java/com/pss/djmw_android/repository/PostRepository.kt
@@ -2,6 +2,7 @@ package com.pss.djmw_android.repository
 
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.pss.djmw_android.data.model.Post
 import com.pss.djmw_android.data.model.UserInfo
 import javax.inject.Inject
@@ -11,7 +12,7 @@ class PostRepository @Inject constructor(
     private val firestore: FirebaseFirestore
 ) {
     //게시판 게시물 가져오기
-    fun getPost() = firestore.collection("post").get()
+    fun getPost() = firestore.collection("post").orderBy("date", Query.Direction.DESCENDING).get()
 
     //게시판 게시물 작성하기
     fun setPost(post : Post) = firestore.collection("post").document().set(post)
