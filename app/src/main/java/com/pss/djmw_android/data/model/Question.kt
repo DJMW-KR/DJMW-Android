32cdb9e3dea3572fa1e4ccb2d034d1240334403d
ParkSangSun <appleasz1028102@gmail.com>
2021-12-15 09:59:27 +0900

    update : Question data class

--- app/src/main/java/com/pss/djmw_android/data/model/Question.kt
+++ app/src/main/java/com/pss/djmw_android/data/model/Question.kt
@@ -11,9 +11,8 @@ data class Question(
     var answer_three : String,
     var answer_four : String,
     var answer_five : String,
-    var question : String,
-    var people : String
-) : Parcelable
+    var question : String
+    ) : Parcelable
 {
-    constructor() : this("오류가 발생했습니다", "오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다")
+    constructor() : this("오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다","오류가 발생했습니다")
 }
