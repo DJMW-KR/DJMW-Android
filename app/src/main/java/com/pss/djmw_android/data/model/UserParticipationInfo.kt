cd752a5939da8ed7022bf6b3a72aa57845a2be73
ParkSangSun <appleasz1028102@gmail.com>
2021-12-20 11:08:39 +0900

    add : UserParticipationInfo data class

---
+++ app/src/main/java/com/pss/djmw_android/data/model/UserParticipationInfo.kt
@@ -0,0 +1,15 @@
+package com.pss.djmw_android.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserParticipationInfo(
+    val q1 : Boolean,
+    val q2 : Boolean,
+    val q3 : Boolean,
+    val q4 : Boolean,
+    val q5 : Boolean
+) : Parcelable {
+    constructor() : this(true,true,true,true,true)
+}
